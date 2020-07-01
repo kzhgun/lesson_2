15c3d35513243c82ac49a688feab73a58583511f
Ksu <zhgunksenia@gmail.com>
2020-07-01 11:46:16 +0300

    Initial commit

---
+++ src/lesson1/Main.java
@@ -0,0 +1,28 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введите операцию :");
+        System.out.println("1. Сложение:");
+        System.out.println("2. Вычитание");
+        System.out.println("3. Умножение");
+        Scanner scanner = new Scanner(System.in);
+        int operation = scanner.nextInt() ;
+        System.out.println("Введите певое число");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int b = scanner.nextInt();
+        int result;
+        if (operation == 1) {
+            result = a + b;
+        } else if(operation == 2){
+            result = a - b;
+        } else {
+            result = a*b;
+        }
+        System.out.println("Результат = " + result);
+    }
+}
